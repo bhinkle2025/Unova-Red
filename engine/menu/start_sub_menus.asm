763e7ac2912da7b155f57aa416d5e19501a3fc61
David Volovskiy <devolov@gmail.com>
2025-01-27 07:46:58 -0500

    Simplified Item Sorting

--- engine/menu/start_sub_menus.asm
+++ engine/menu/start_sub_menus.asm
@@ -307,7 +307,6 @@ StartMenu_Item:
 .notInCableClubRoom
 	ld hl,wFlags_0xcd60
 	set 2,[hl]
-	res 4,[hl]
 	ld bc,wNumBagItems
 	ld hl,wListPointer
 	ld a,c
@@ -327,7 +326,6 @@ StartMenu_Item:
 .exitMenu
 	ld hl,wFlags_0xcd60
 	res 2,[hl]
-	res 4,[hl]
 	call LoadScreenTilesFromBuffer2 ; restore saved screen
 	call LoadTextBoxTilePatterns
 	call UpdateSprites
